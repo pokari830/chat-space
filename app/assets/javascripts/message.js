a980330ad07c8631d5ca0f2579510f4cfbaec4c3
中西泰朗 <yasuo.nakanishi.0930@gmail.com>
2020-05-08 13:01:19 +0900

    message Ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+$(function(){
+
+  function buildHTML(message){
+    if (message.image){
+      var html = `<div class="main__contents__chat">
+                    <div class="main__contents__chat__user">
+                      ${message.user_name}
+                    </div>
+                    <div class= "main__contents__chat__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class= "main__contents__chat__message">
+                    ${message.text}
+                    <img src=${message.image} >
+                  </div>`
+    }else{
+      var html = `<div class="main__contents__chat">
+                    <div class="main__contents__chat__user">
+                      ${message.user_name}
+                    </div>
+                    <div class= "main__contents__chat__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class= "main__contents__chat__message">
+                    ${message.text}
+                  </div>`
+    }
+    return html
+  }
+
+  $(".new_message").on("submit", function(e){
+    e.preventDefault();
+    $(".send__btn").removeAttr("data-disable-with");
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".main__contents").append(html);
+      $(".main__contents").animate({scrollTop: $(".main__contents")[0].scrollHeight});
+      $("form")[0].reset();
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    });
+  })
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # def after_sign_in_path_for(resource)
@@ -16,4 +16,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
 end
