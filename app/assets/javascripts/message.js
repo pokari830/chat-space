73cb2f383bdbf7fb83f845ce49ad6a65b259eee8
中西泰朗 <yasuo.nakanishi.0930@gmail.com>
2020-05-15 20:01:22 +0900

    autoReload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,32 +2,37 @@ $(function(){
 
   function buildHTML(message){
     if (message.image){
-      var html = `<div class="main__contents__chat">
-                    <div class="main__contents__chat__user">
-                      ${message.user_name}
+      var html = `<div class="message" data-message-id=${message.id}>
+                    <div class="main__contents__chat">
+                      <div class="main__contents__chat__user">
+                        ${message.user_name}
+                      </div>
+                      <div class= "main__contents__chat__date">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class= "main__contents__chat__date">
-                      ${message.created_at}
+                    <div class= "main__contents__chat__message">
+                      ${message.text}
+                      <img src=${message.image} >
                     </div>
-                  </div>
-                  <div class= "main__contents__chat__message">
-                    ${message.text}
-                    <img src=${message.image} >
                   </div>`
+      return html;
     }else{
-      var html = `<div class="main__contents__chat">
-                    <div class="main__contents__chat__user">
-                      ${message.user_name}
+      var html = `<div class="message" data-message-id=${message.id}>
+                    <div class="main__contents__chat">
+                      <div class="main__contents__chat__user">
+                        ${message.user_name}
+                      </div>
+                      <div class= "main__contents__chat__date">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class= "main__contents__chat__date">
-                      ${message.created_at}
+                    <div class= "main__contents__chat__message">
+                      ${message.text}
                     </div>
-                  </div>
-                  <div class= "main__contents__chat__message">
-                    ${message.text}
                   </div>`
-    }
-    return html
+      return html;
+    };
   }
 
   $(".new_message").on("submit", function(e){
@@ -53,5 +58,33 @@ $(function(){
     .fail(function(){
       alert("メッセージ送信に失敗しました");
     });
+
   })
+  var reloadMessages = function(){
+    var last_message_id = $(".message:last").data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insertHTML = "";
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        console.log("更新成功");
+        $(".main__contents").append(insertHTML);
+        $(".main__contents").animate({ scrollTop: $(".main__contents")[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert("error");
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+  };
+
 });
