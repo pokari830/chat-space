dbfadb6e9832a7d0f82f5b0b7c58fc0b5ddc1405
中西泰朗 <yasuo.nakanishi.0930@gmail.com>
2020-06-03 11:57:55 +0900

    autoReload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,9 +10,8 @@ $(function(){
                       <div class= "main__contents__chat__date">
                         ${message.created_at}
                       </div>
-                    </div>
-                    <div class= "main__contents__chat__message">
-                      ${message.text}
+                      <div class= "main__contents__chat__message">
+                        ${message.text}
                       <img src=${message.image} >
                     </div>
                   </div>`
@@ -23,12 +22,9 @@ $(function(){
                       <div class="main__contents__chat__user">
                         ${message.user_name}
                       </div>
-                      <div class= "main__contents__chat__date">
-                        ${message.created_at}
+                      <div class= "main__contents__chat__message">
+                        ${message.text}
                       </div>
-                    </div>
-                    <div class= "main__contents__chat__message">
-                      ${message.text}
                     </div>
                   </div>`
       return html;
@@ -41,49 +37,31 @@ $(function(){
     var formData = new FormData(this);
     var url = $(this).attr("action");
 
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $(".main__contents").append(html);
-      $(".main__contents").animate({scrollTop: $(".main__contents")[0].scrollHeight});
-      $("form")[0].reset();
-    })
-    .fail(function(){
-      alert("メッセージ送信に失敗しました");
-    });
-
-  })
-  var reloadMessages = function(){
-    var last_message_id = $(".message:last").data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: "get",
-      dataType: "json",
-      data: {id: last_message_id}
-    })
-    .done(function(messages){
-      if (messages.length !== 0){
-        var insertHTML = "";
-        $.each(messages, function(i, message){
-          insertHTML += buildHTML(message)
-        });
-        $(".main__contents").append(insertHTML);
-        $(".main__contents").animate({ scrollTop: $(".main__contents")[0].scrollHeight});
-      }
-    })
-    .fail(function(){
-      alert("error");
-    });
-  };
-  if (document.location.href.match(/\/groups\/\d+\/messages/)){
-    setInterval(reloadMessages, 7000);
-  };
-
+    var reloadMessages = function(){
+      var last_message_id = $(".message:last").data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: "get",
+        dataType: "json",
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        if (messages.length !== 0){
+          var insertHTML = "";
+          $.each(messages, function(i, message){
+            insertHTML += buildHTML(message)
+          });
+          console.log("更新成功");
+          $(".main__contents").append(insertHTML);
+          $(".main__contents").animate({ scrollTop: $(".main__contents")[0].scrollHeight});
+        }
+      })
+      .fail(function(){
+        alert("error");
+      });
+    };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)){
+      setInterval(reloadMessages, 7000);
+    };
+  });
 });
