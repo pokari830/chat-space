e10664d44602f21818ebc10c579bfdcefe0cf162
中西泰朗 <yasuo.nakanishi.0930@gmail.com>
2020-06-04 05:24:28 +0900

    addApiController

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,6 +36,9 @@ $(function(){
   }
 
   $(".new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
     var reloadMessages = function(){
       var last_message_id = $(".message:last").data("message-id");
       $.ajax({
