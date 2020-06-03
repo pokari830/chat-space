360452da9813c228a59e53b09d85336180b6296a
中西泰朗 <yasuo.nakanishi.0930@gmail.com>
2020-06-03 12:52:24 +0900

    reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,62 +1,65 @@
 $(function(){
-  var last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
 
   function buildHTML(message){
     if (message.image){
-      var html = `<div class="main__contents__chat">
-                    <div class="main__contents__chat__user">
-                      ${message.user_name}
+      var html = `<div class="message" data-message-id=${message.id}>
+                    <div class="main__contents__chat">
+                      <div class="main__contents__chat__user">
+                        ${message.user_name}
+                      </div>
+                      <div class= "main__contents__chat__date">
+                        ${message.created_at}
+                      </div>
+                      <div class= "main__contents__chat__message">
+                        ${message.text}
+                      <img src=${message.image} >
                     </div>
-                    <div class= "main__contents__chat__date">
-                      ${message.created_at}
-                    </div>
-                  </div>
-                  <div class= "main__contents__chat__message">
-                    ${message.text}
-                    <img src=${message.image} >
                   </div>`
+      return html;
     }else{
-      var html = `<div class="main__contents__chat">
-                    <div class="main__contents__chat__user">
-                      ${message.user_name}
+      var html = `<div class="message" data-message-id=${message.id}>
+                    <div class="main__contents__chat">
+                      <div class="main__contents__chat__user">
+                        ${message.user_name}
+                      </div>
+                      <div class= "main__contents__chat__date">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class= "main__contents__chat__date">
-                      ${message.created_at}
+                      <div class= "main__contents__chat__message">
+                        ${message.text}
+                      </div>
                     </div>
-                  </div>
-                  <div class= "main__contents__chat__message">
-                    ${message.text}
                   </div>`
-    }
-    return html
+      return html;
+    };
   }
 
   $(".new_message").on("submit", function(e){
-    e.preventDefault();
-    $(".send__btn").removeAttr("data-disable-with");
-    var formData = new FormData(this);
-    var url = $(this).attr("action");
-
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $(".main__contents").append(html);
-      $(".main__contents").animate({scrollTop: $(".main__contents")[0].scrollHeight});
-      $("form")[0].reset();
-    })
-    .fail(function(){
-      alert("メッセージ送信に失敗しました");
-    });
+    var reloadMessages = function(){
+      var last_message_id = $(".message:last").data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: "get",
+        dataType: "json",
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        if (messages.length !== 0){
+          var insertHTML = "";
+          $.each(messages, function(i, message){
+            insertHTML += buildHTML(message)
+          });
+          $(".main__contents").append(insertHTML);
+          $(".main__contents").animate({ scrollTop: $(".main__contents")[0].scrollHeight});
+        }
+      })
+      .fail(function(){
+        alert("error");
+      });
+    };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)){
+      setInterval(reloadMessages, 7000);
+    };
   })
-  if (document.location.href.match(/\/groups\/\d+\/messages/)){
-    setInterval(reloadMessage, 7000);
-  }
 });
