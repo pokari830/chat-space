61a8d9d66f8bb2b645e8ccf4fa81f1d52cb346d5
中西泰朗 <yasuo.nakanishi.0930@gmail.com>
2020-06-08 02:13:43 +0900

    autoReload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,7 +35,7 @@ $(function(){
   }
 
   $(".new_message").on("submit", function(e){
-    e.preventDefault();
+    e.preventDefault(); //デフォルトのフォーム処理を無効化する
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -46,36 +46,40 @@ $(function(){
       processData: false,
       contentType: false
     })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.main__contents').append(html);
-        $('.main__contents').animate({ scrollTop: $('.main__contents')[0].scrollHeight});
-        $('form')[0].reset();
-      })
-    var reloadMessages = function(){
-      var last_message_id = $(".message:last").data("message-id");
-      $.ajax({
-        url: "api/messages",
-        type: "get",
-        dataType: "json",
-        data: {id: last_message_id}
-      })
-      .done(function(messages){
-        if (messages.length !== 0){
-          var insertHTML = "";
-          $.each(messages, function(i, message){
-            insertHTML += buildHTML(message)
-          });
-          $(".main__contents").append(insertHTML);
-          $(".main__contents").animate({ scrollTop: $(".main__contents")[0].scrollHeight});
-        }
-      })
-      .fail(function(){
-        alert("error");
-      });
-    };
-    if (document.location.href.match(/\/groups\/\d+\/messages/)){
-      setInterval(reloadMessages, 7000);
-    };
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main__contents').append(html); //main__contentsの最後に要素を追加する
+      $('.main__contents').animate({ scrollTop: $('.main__contents')[0].scrollHeight}); //画面を最下部に遷移する
+      $('form')[0].reset(); //フォーム内の入力をリセットする
+      $('.send__btn').attr('disabled', false); //btnを有効にする
+    })
+    .fail(function(){
+      alert("error");
+    });
   })
+  var reloadMessages = function(){
+    var last_message_id = $(".message:last").data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insertHTML = "";
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $(".main__contents").append(insertHTML);
+        $(".main__contents").animate({ scrollTop: $(".main__contents")[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert("error");
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+  };
 });
